b302e8b194a2ea9c075a0ffcc599140a43f6e73c
Serhiy Hlavatskiy <foegit@gmail.com>
2019-05-30 16:02:12 +0300

    Restore mutation.js

---
+++ src/store/mutations.js
@@ -0,0 +1,16 @@
+export default {
+  updateUsers(state, payload) {
+    state.users = payload.userList;
+  },
+  startLoadResource(state) {
+    state.loading = true;
+  },
+  stopLoadResource(state) {
+    state.loading = false;
+  },
+  errorOccured(state, payload) {
+    // console.log('error', payload.error);
+
+    state.errors = [...state.errors, payload.error];
+  }
+}

721d23cf9d9fa2a335beb0df80e11d5ccb50bd3d
Bachu M <bachu_misha@mail.ru>
2019-06-07 14:36:01 +0300

    created mock bank

---
+++ src/mocks/bank.js
@@ -0,0 +1,115 @@
+const data =[
+  {
+    id_subject: 1,
+    name: 'Алгебра та геометрія',
+    questions: [
+      {
+        id_question: 1,
+        question_title: 'Сколько зубов у человека?',
+        answer_title: 'сім'
+      },
+      {
+        id_question: 2,
+        question_title: 'Як тебе звати?',
+        answer_title: 'Петро'
+      },
+      {
+        id_question: 3,
+        question_title: 'Знайдіть похідну функції у(х)=2х ?',
+        answer_title: 'не можу'
+      },
+      {
+        id_question: 4,
+        question_title: 'Ви можете множити матриці?',
+        answer_title: 'Канешна'
+      },
+      {
+        id_question: 5,
+        question_title: 'Скількі літрів в 5 метрах дороги?',
+        answer_title: 'три з черепахою'
+      },
+      {
+        id_question: 6,
+        question_title: 'Як знайти банан',
+        answer_title: 'банани священні і їх неможна шукати'
+      },
+      {
+        id_question: 7,
+        question_title: 'Чи можна купити лопату?',
+        answer_title: 'ну лопату нельзя, купіть краще слона'
+      },
+      {
+        id_question: 8,
+        question_title: 'Хочеш анекдот?',
+        answer_title: 'а він тебе не хоче'
+      },
+      {
+        id_question: 9,
+        question_title: 'Знайти подвійний інтеграл від границі Ляпунова.',
+        answer_title: 'Я що схожий на абонента ?'
+      },
+      {
+        id_question: 10,
+        question_title: 'Бать все нормально?',
+        answer_title: 'Нармана'
+      }
+    ]
+  },
+  {
+    id_subject: 2,
+    name: 'Методи обчислень',
+    questions: [
+      {
+        id_question: 1,
+        question_title: 'Як звaти бєтмена',
+        answer_title: 'сім'
+      },
+      {
+        id_question: 2,
+        question_title: 'Знaйти подвiйний інтигрований добуток з суми інших черепах?',
+        answer_title: '11'
+      },
+      {
+        id_question: 3,
+        question_title: 'Знайти квазілінійний простір ортогональних кілець Мебіуса',
+        answer_title: 'в мене лапки замість рук, дайте це завдання іншому'
+      },
+      {
+        id_question: 4,
+        question_title: 'Скільки потрібно відкрутити гайок щоб вийшла черепаха?',
+        answer_title: 'десь 5-6 пятого, 7-8 пятого'
+      },
+      {
+        id_question: 5,
+        question_title: 'Скільки літрів спирту потрібно щоб напоїти маленького котика?',
+        answer_title: '144'
+      },
+      {
+        id_question: 6,
+        question_title: 'Чому дорівнює корень з пяти?',
+        answer_title: 'мабуть 11'
+      },
+      {
+        id_question: 7,
+        question_title: 'А це пямятник свині',
+        answer_title: 'ніт'
+      },
+      {
+        id_question: 8,
+        question_title: 'У кого з рогатої худоби пять хвостів',
+        answer_title: 'у пятихвостої корови'
+      },
+      {
+        id_question: 9,
+        question_title: 'Чому свині такі свині? ',
+        answer_title: 'А що ви ще від свиней хотіли'
+      },
+      {
+        id_question: 10,
+        question_title: 'Назвіть формулу води',
+        answer_title: 'Аш2О'
+      }
+    ]
+  },
+]
+export default data;
